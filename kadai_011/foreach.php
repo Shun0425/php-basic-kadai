21872a10dbda990a70330d080cd4ad1efe87b611
Shun0425 <49826083+Shun0425@users.noreply.github.com>
2025-02-20 15:18:26 +0900

    kadai_011の追加

---
+++ kadai_011/foreach.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>foreach文を使って連想配列の値とキーを出力しよう</title>
+</head>
+<body>
+    <?php
+    $Array = [
+        "名前" => "玉ねぎ",
+        "値段" => 200,
+        "産地" => "北海道",
+    ];
+    foreach ($Array as $key => $value) {
+        echo $key . "：" . $value . "<br>";
+    }
+    ?>
+</body>
+</html>

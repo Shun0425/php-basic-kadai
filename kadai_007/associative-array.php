13798c9bef5bf4dab40f03c036a3ed3b940a2866
Shun0425 <49826083+Shun0425@users.noreply.github.com>
2025-02-20 12:45:44 +0900

    kadai_007の追加

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>連想配列を作ってキーと値を出力しよう</title>
+</head>
+<body>
+    <?php
+    $Array = [
+        "name" => "onion",
+        "price" => 200,
+        "weight" => 160,
+    ];
+    print_r($Array);
+    ?>
+</body>
+</html>
